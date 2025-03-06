a2d105654c5d07d5f6ed84dd8588c0e6d9499bec
Darien Bernal <darien.bernal@gmail.com>
2025-03-06 17:36:32 -0500

    Completed form and Clueless renders as the first movie

--- react-movie-search/src/App.jsx
+++ react-movie-search/src/App.jsx
@@ -1,5 +1,4 @@
 import {useState, useEffect} from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 import MovieDisplay from "./components/MovieDisplay";
@@ -12,23 +11,23 @@ export default function App() {
   // State to hold movie data
   const [movie, setMovie] = useState(null);
 
-  // Function to get movies
-  const getMovie = async(searchTerm) => {
-    // Make fetch request and store the response
+  const getMovie = async (searchTerm) => {
     const response = await fetch(
       `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
     );
-    // Parse JSON response into a JavaScript object
     const data = await response.json();
-    // Set the Movie state to the received data
     setMovie(data);
   };
 
-  // We pass the getMovie function as a prop called moviesearch
+  // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie(randomMovie);
+  }, []);
+
   return (
     <div className="App">
       <Form moviesearch={getMovie} />
-      <MovieDisplay />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }

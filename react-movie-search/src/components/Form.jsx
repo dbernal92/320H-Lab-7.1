ba82a17106b00dd0820774b9de90d03257a7123a
Darien Bernal <darien.bernal@gmail.com>
2025-03-06 16:50:33 -0500

    Initial commit

---
+++ react-movie-search/src/components/Form.jsx
@@ -0,0 +1,10 @@
+export default function Form(props) {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    );
+  }
